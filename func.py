f1e8d66454c85129c86a55d9d0d053cfe9fdc814
Eyvaz Najafli <eyvaz.necefli.std@bhos.edu.az>
2020-01-17 10:24:15 +0400

    Modified

--- func.py
+++ func.py
@@ -1,14 +1,18 @@
 def func1(element):
-    r = []
+    # r = []
+    print('Func1 is called')
     for i in element:
-        if(i % 2 == 0):
-            r.append(i)
-    return r
+        if(i !=0 and i % 2 == 0):
+            # r.append(i)
+            print(i)
+    # return r
 
 
 def func2(element):
-    r = []
+    # r = []
+    print('Func2 is called')
     for i in element:
-        if(bin(i)[2:] == '0'):
-            r.append(i)
-    return r
+        if(i != 0 and bin(i)[2:][len(bin(i)[2:]) - 1] == '0'):
+            # r.append(i)
+            print(i)
+    # return r
--- session1.py
+++ session1.py
@@ -4,17 +4,17 @@ from func import func1, func2
 # import matplotlib.pyplot as plt
 from datetime import datetime
                         # Max Limit 10000000
-a = np.random.randint(100, size = 1000)  
+a = np.random.randint(100, size = 1000)   # Normal Size
 # print(a)
 # a = 1000000
 t0 = datetime.now() 
-result1 = func1(a)
+func1(a)
 dt1 = datetime.now() - t0
 
 t0 = datetime.now()
-result2 = func2(a)
+func2(a)
 dt2 = datetime.now() - t0
-print(result1)
-print(result2)
+# print(result1)
+# print(result2)
 print('dt1 -> ', dt1.total_seconds())
 print('dt2 -> ', dt2.total_seconds())
